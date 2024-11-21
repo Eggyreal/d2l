156bbc5da54d77dd97bbc22f54d3048ee94c948f
Benjamin Weng <108381704+Eggyreal@users.noreply.github.com>
2024-11-22 00:23:12 +0800

    Update net.py

--- net.py
+++ net.py
@@ -1,8 +1,10 @@
 import torch
 
-class net(nn.Module):
+X = torch.randn(X,dtype = torch.float)
+#构造网络层
+class Net(nn.Module):
     def __init__(self):
-        super(net, self).__init__()
+        super(Net, self).__init__()
         self.fc1 = nn.Linear(10, 20)
         self.relu = nn.ReLU()
         self.fc2 = nn.Linear(20, 1)
@@ -12,3 +14,15 @@ class net(nn.Module):
         x = self.relu(x)
         x = self.fc2(x)
         return x
+net = Net(X)
+#顺序块，李沐傻逼写着炫技的，还不如像上面老老实实地写
+class MySequential(nn.Module):
+    def __init__(self,*args):
+        super().__init__()
+        for block in args:
+            self._modules[block] = block
+    def forward(self, X):
+        for block in self._modules.values():
+            X = block(X)
+        return X
+net = MySequential(nn.Linear(20,256), nn.ReLU(), nn.Linear(256,10))
